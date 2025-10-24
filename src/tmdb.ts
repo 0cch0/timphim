f316669ddb003c6f83c61fad2bdcb67c12b84333
0cch0 <89780085+0cch0@users.noreply.github.com>
2025-10-24 17:29:51 +0700

    wip

--- src/tmdb.ts
+++ src/tmdb.ts
@@ -1,6 +1,7 @@
 export function fetchTMDB(path: string, query?: Record<string, string | number>) {
 	const baseUrl = 'https://api.themoviedb.org/3'
-	const url = new URL(`${baseUrl}${path}`)
+	const _path = path[0] == '/' ? path : '/' + path
+	const url = new URL(`${baseUrl}${_path}`)
 
 	url.search = new URLSearchParams({
 	api_key: '5fc32caa702eec5a5336f8aa37f064c7',
