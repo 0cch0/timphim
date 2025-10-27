38b07458f077f401df3690a39c88c349cf5365d1
0cch0 <89780085+0cch0@users.noreply.github.com>
2025-10-27 11:10:28 +0700

    wip

--- src/yts.ts
+++ src/yts.ts
@@ -1,4 +1,6 @@
 function get(imdb_id) {
 	const url = 'https://yts.mx/api/v2/movie_details.json'
-	return fetch(`${url}?imdb_id=${imdb_id}`)
+	return fetch(`${url}?imdb_id=${imdb_id}`).then(res => res.text())
 }
+
+export default { get }
